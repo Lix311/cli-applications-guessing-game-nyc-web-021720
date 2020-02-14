dcd28105f379ba64d5d6ffe33bfbb3890fb6dc88
Alex Sterrantino <alexsterrantino@gmail.com>
2020-02-14 23:38:59 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -7,7 +7,7 @@ def run_guessing_game
     elsif random_number == user_input
       puts "You guessed the correct number!"
     elsif random_number != user_input
-      puts "Sorry! The computer guessed #{random_number}"
+      print "Sorry! The computer guessed #{random_number}"
     end 
 end 
 
