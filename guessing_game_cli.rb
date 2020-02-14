abff68d42e06d9d9839d380cc823fb21784afbc0
Alex Sterrantino <alexsterrantino@gmail.com>
2020-02-14 23:52:35 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -3,7 +3,6 @@ def run_guessing_game
   random_number = 1 + rand(6)
   puts "Enter a number 1 through 6"
   user_input = gets.chomp
-    binding.pry 
     if user_input == "exit"
       puts "Goodbye!"
     elsif random_number == user_input
